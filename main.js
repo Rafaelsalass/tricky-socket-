3ffb4be133db299aa9adcf5f41c611c28a0b1b2e
devheraclea <dev@heracleaweb.com>
2019-07-25 14:58:43 -0500

    i dont know

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // express for creating the server
-import express from 'express';
+import express from "express";
 // enable http communcation
-import http from 'http';
+import http from "http";
 // create and manage sockets
-import io from 'socket.io';
+import io from "socket.io";
 
-import uuidv1 from 'uuid/v1';
+import uuidv1 from "uuid/v1";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -14,20 +14,20 @@ const socketIo = io(httpServer);
 let users = {};
 let games = {};
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("<h1>Hello im the socket sever, I'm runing happily</h1>");
-})
+});
 
-socketIo.on('connection', socket => {
+socketIo.on("connection", socket => {
   console.log(`user have connected: ${socket.id}`);
 
   // update users array adding the new user
-  users = { ...users, [socket.id]: {name: ''}};
+  users = { ...users, [socket.id]: { name: "" } };
 
   // broadcast users to all clients
-  socketIo.emit('user_update', users);
+  socketIo.emit("user_update", users);
 
-  socket.on('disconnect', function(){
+  socket.on("disconnect", function() {
     console.log(`user have disconnected: ${socket.id}`);
 
     // removing the user that is disconnecting
@@ -35,24 +35,27 @@ socketIo.on('connection', socket => {
     console.log(users);
 
     // broadcast users to all clients
-    socketIo.emit('user_update', users);
+    socketIo.emit("user_update", users);
   });
 
-  socket.on('name_update', (user_name) => {
+  socket.on("name_update", user_name => {
     // update user name
-    users = { ...users, [socket.id]: {
-      ...users[socket.id],
-      name: user_name
-    }};
+    users = {
+      ...users,
+      [socket.id]: {
+        ...users[socket.id],
+        name: user_name
+      }
+    };
 
     // broadcast users to all clients
-    socketIo.emit('user_update', users);
+    socketIo.emit("user_update", users);
   });
 
-  socket.on('create_game', () => {
+  socket.on("create_game", () => {
     let gameAlreadyExist = false;
     Object.keys(games).map(key => {
-      if(games[key].creator === socket.id) {
+      if (games[key].creator === socket.id) {
         gameAlreadyExist = true;
       }
     });
@@ -63,16 +66,16 @@ socketIo.on('connection', socket => {
           creator: socket.id,
           playerOne: socket.id,
           gameStatus: {
-            status: 'waiting on player two',
-          },
+            status: "waiting on player two"
+          }
         }
-      }
-      socketIo.emit('game_update', games);
+      };
+      socketIo.emit("game_update", games);
     }
   });
 });
 
 const PORT = 8000;
 httpServer.listen(PORT, () => {
- console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
